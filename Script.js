ca0e345f8d57f787942c5cee61c825f7489e7def
earljames052 <earljamescabacungan81@gmail.com>
2025-03-26 16:45:43 +0800

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,40 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+}
+
+table {
+    width: 60%;
+    margin: auto;
+    border-collapse: collapse;
+}
+
+th, td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: center;
+}
+
+th {
+    background-color:rgb(56, 204, 51);
+    color: white;
+}
+
+button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    background-color: green;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: darkgreen;
+}
+
+/* New style to be applied when button is clicked */
+.new-style {
+    background-color: black;
+    color: white;
+}
